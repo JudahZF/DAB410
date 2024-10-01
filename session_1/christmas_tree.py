a85fbf5ae231afdc0463fc0f474477c39fa702ee
Judah Fuller <judahfuller@icloud.com>
2024-10-01 15:31:52 +0100

    Add comments

--- session_1/christmas_tree.py
+++ session_1/christmas_tree.py
@@ -3,7 +3,7 @@ import math
 # Get the tree height
 tree_height = int(input("Enter the height of the tree: "))
 
-# Print the top of the tree
+# Print the top of the border
 print(f"{"_"*((4*tree_height)+9)}")
 
 for i in range(tree_height):
@@ -12,6 +12,8 @@ for i in range(tree_height):
 
 
 for i in range(math.floor(tree_height/5)):
-    print(f"| {" "*(2*tree_height-math.floor(tree_height/10))}|{" "*math.floor(tree_height)}|{" "*(2*tree_height-math.floor(tree_height/10))} |")
+    # Print the tree trunk
+    print(f"| {" "*(2*tree_height-math.ceil(tree_height/10)+2)}|{" "*math.floor(tree_height/5)}|{" "*(2*tree_height-math.ceil(tree_height/10)+2)} |")
 
+# Print the bottom of the border
 print(f"{"_"*((4*tree_height)+9)}")
